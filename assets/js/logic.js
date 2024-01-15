1d8ac69d13de7356715d91503e603259f6a3532c
louie888a <alexlouie@ymail.com>
2024-01-15 11:58:58 +0000

    designing questions js

---
+++ assets/js/logic.js
@@ -0,0 +1,13 @@
+const scores = document.getElementById("#scores");
+const timer = document.getElementById("#timer");
+const startScreen = document.getElementById("#start-screen");
+const start = document.getElementById("#start");
+const questions = document.getElementById("#questions");
+const choices = document.getElementById("#choices");
+const finalScore = document.getElementById("#finalScore");
+const initials = document.getElementById("#initials");
+const submit = document.getElementById("#submit");
+const feedBack = document.getElementById("#feedback");
+
+const correctTune = new Audio("./assets/sfx/correct.wav");
+const incorrectTune = new Audio("./assets/sfx/incorrect.wav");
---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+const quizQuestions = [
+  {
+    question:
+      "Questions 1 : String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes",
+  },
+  {
+    question: "Questions 2 : Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question: "Questions 3 : How do you create a function in JavaScript",
+    choices: [
+      "function = myFunction()",
+      "function myFunction()",
+      "function:myFunction()",
+      "createMyFunction()",
+    ],
+    answer: "function myFunction()",
+  },
+  {
+    question: "Questions 4 : How do you call a function named myFunction?",
+    choices: [
+      "call myFunction()",
+      "call function myFunction()",
+      "myFunction()",
+      "call myFunction",
+    ],
+    answer: "myFunction()",
+  },
+  {
+    question:
+      "Questions 5 : To see if two variables are equal in an if / else statement you would use ____.",
+    choices: ["=", "==", "equals", "!="],
+    answer: "==",
+  },
+  {
+    question: "Questions 6 : The first index of an array is ____.",
+    choices: ["0", "1", "8", "any"],
+    answer: "0",
+  },
+  {
+    question: "Questions 7 : How to write an IF statement in JavaScript?",
+    choices: ["if i == 5 then", "if i = 5 then", "if(i == 5)", "if i = 5"],
+    answer: "if(i == 5)",
+  },
+  {
+    question:
+      "Questions 8 : Which event occurs when the user clicks on an HTML element?",
+    choices: ["onclick", "onchange", "onmouseover", "onmouseclick"],
+    answer: "onclick",
+  },
+];
+
+console.log(quizQuestions);
